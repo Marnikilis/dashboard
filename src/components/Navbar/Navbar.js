b724cabad989f56cd6da7494a0ca7dad2a997fc5
Marnikilis <diana@cravter.com>
2022-05-18 13:29:52 +0300

    add drawer

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React,{useState} from 'react';
 import styles from './Navbar.module.scss';
 import {NavLink} from "react-router-dom";
 import Logo from "./Logo";
@@ -10,41 +10,56 @@ import {ReactComponent as Activity} from '../../images/icons/activity.svg';
 import {ReactComponent as Analytics} from '../../images/icons/analytics.svg';
 import {ReactComponent as Help} from '../../images/icons/help.svg';
 import {ReactComponent as Settings} from '../../images/icons/settings.svg';
+import MenuToggle from "./MenuToggle/MenuToggle";
+import Backdrop from "./Backdrop/Backdrop";
 
 
 const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenuHandler = () => {
+    setIsOpen(prevState => !prevState)
+  }
+  const menuCloseHandler = () => {
+    setIsOpen(false)
+  }
+
+  const cls = [styles.nav];
+  if (!isOpen) {
+    cls.push(styles.close)
+  }
   return (
-    <nav className={styles.container}>
-        <Logo />
-        <div className={styles.menu}>
-          <input className={styles.checkbox} type='checkbox' id='check'/>
-          <div className={styles.menuBtn}/>
-          <div className={styles.nav}>
-          <div className={styles.navLinks}>
-            <NavLink to='/dashboard'>
-              <Home className={'icon'}/>Dashboard</NavLink>
-            <NavLink to='/invoices'>
-              <Invoice className={'icon'}/> Invoices</NavLink>
-            <NavLink to='/messages'>
-              <Message className={'icon'}/>Messages
-              <span className={styles.messages}>5</span>
-            </NavLink>
-            <NavLink to='/wallets'>
-              <Wallet className={'icon'}/>My Wallets</NavLink>
-            <NavLink to='/activity'>
-              <Activity className={'icon'}/>Activity</NavLink>
-            <NavLink to='/analytics'>
-              <Analytics className={'icon'}/>Analytics</NavLink>
+    <>
+      <div className={styles.container}>
+          <Logo/>
+        <div className={cls.join(' ')}>
+            <div className={styles.navLinks}>
+              <NavLink to='/dashboard'>
+                <Home className={'icon'}/>Dashboard</NavLink>
+              <NavLink to='/invoices'>
+                <Invoice className={'icon'}/> Invoices</NavLink>
+              <NavLink to='/messages'>
+                <Message className={'icon'}/>Messages
+                <span className={styles.messages}>5</span>
+              </NavLink>
+              <NavLink to='/wallets'>
+                <Wallet className={'icon'}/>My Wallets</NavLink>
+              <NavLink to='/activity'>
+                <Activity className={'icon'}/>Activity</NavLink>
+              <NavLink to='/analytics'>
+                <Analytics className={'icon'}/>Analytics</NavLink>
+            </div>
+            <div className={styles.navSettings}>
+              <NavLink to='/help'>
+                <Help className={'icon'}/>Get Help</NavLink>
+              <NavLink to='/settings'>
+                <Settings className={'icon'}/>Settings</NavLink>
+            </div>
           </div>
-          <div className={styles.navSettings}>
-            <NavLink to='/help'>
-              <Help className={'icon'}/>Get Help</NavLink>
-            <NavLink to='/settings'>
-              <Settings className={'icon'}/>Settings</NavLink>
-          </div>
-          </div>
-        </div>
-    </nav>
+          <MenuToggle onToggle={toggleMenuHandler} isOpen={isOpen}/>
+        {isOpen && <Backdrop onClick={menuCloseHandler}/>}
+      </div>
+    </>
   );
 };
 
