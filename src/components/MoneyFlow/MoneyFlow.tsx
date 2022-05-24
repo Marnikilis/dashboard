5ee40d7242ea22c7b8e22310bcb28e0fb9dc367b
Marnikilis <diana@cravter.com>
2022-05-24 14:01:36 +0300

    -

--- src/components/MoneyFlow/MoneyFlow.tsx
+++ src/components/MoneyFlow/MoneyFlow.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 //@ts-ignore
 import styles from './MoneyFlow.module.scss';
-import {ReactComponent as Graphic} from "../../images/graphic.svg";
-import {ReactComponent as GraphImg} from "../../images/graphImg.svg";
+import { ReactComponent as Graphic } from "../../images/graphic.svg";
+import { ReactComponent as GraphImg } from "../../images/graphImg.svg";
 import Arrow from "./Arrow";
 import CustomSelect from '../../containers/CustomSelect/CustomSelect';
 import { ReactComponent as ArrowBtn } from '../../images/arrowBtn.svg';
@@ -23,7 +23,7 @@ const MoneyFlow = () => {
           <Arrow/>
         </div>
         <span>Savings</span>
-        <div> <Arrow color={'#0177FD'}/> +6,79% </div>
+        <div><Arrow color={'#0177FD'}/> +6,79%</div>
         <div className={styles.select}>
           <CustomSelect options={optionsPeriod}>
             <ArrowBtn/>
